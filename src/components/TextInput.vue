f3bebaff416e18cea0033fbc395f2ee781ab71e4
yin <yintothayang@gmail.com>
2018-07-19 22:06:53 -0700

    the most mvp mvp

---
+++ src/components/TextInput.vue
@@ -0,0 +1,85 @@
+<template lang="pug">
+#text-input(:class="status")
+  input(v-model="attempt" ref="input" type="text")
+
+
+</template>
+
+<script>
+import {mapActions, mapMutations, mapGetters} from 'vuex'
+export default {
+  name: 'TextInput',
+  props: {
+    card: {
+      default: ()=>{}
+    }
+  },
+  data() {
+    return {
+      attempt: "",
+      focused: true,
+      status: []
+    }
+  },
+  watch: {
+    attempt: function(value){
+      this.status = []
+      if(value == this.card.back){
+        this.status = ['success']
+        this.$emit('success')
+
+        setTimeout(()=>{
+          this.status = []
+          this.attempt = ""
+        }, 100)
+
+      } else if(value.length >= this.card.back.length){
+        this.status = ['fail']
+      }
+
+    }
+  },
+  computed: {
+    ...mapGetters({
+
+    })
+  },
+  methods: {
+    ...mapMutations({
+
+    }),
+
+    onAttempt(attempt){
+
+    }
+  },
+  created(){
+
+  },
+  mounted(){
+    this.$refs.input.focus()
+  }
+}
+</script>
+
+
+<style lang="stylus" scoped>
+#text-input
+  display flex
+  padding .5em 1em
+  background white !important
+  box-shadow -1px 3px 2px 1px rgba(0, 0, 0, .1)
+  font-weight 500
+  font-size 1.5em
+  border 2px solid white
+
+  &.fail
+    border 2px solid red
+
+  &.success
+    border 2px solid lightgreen
+
+
+
+
+</style>
--- src/pages/GamePage.vue
+++ src/pages/GamePage.vue
@@ -3,16 +3,22 @@
   .card-container
     .card
       span {{currentCard.front}}
+  .text-input-container
+    text-input(:card="currentCard" @attempt="onAttempt" @success="onSuccess")
 </template>
 
 <script>
-
+import TextInput from '@/components/TextInput'
 import {mapActions, mapMutations, mapGetters} from 'vuex'
 export default {
   name: 'GamePage',
+  components: {
+    TextInput,
+  },
   data() {
     return {
-      cards: []
+      cards: [],
+      seenCards: [],
     }
   },
   computed: {
@@ -27,6 +33,34 @@ export default {
     ...mapMutations({
       setNavbarTitle: 'navbar/SET_TITLE',
     }),
+    onAttempt(answer){
+      console.log("attempt", answer)
+    },
+    onSuccess(){
+      if(this.cards.length > 1){
+        this.seenCards.push(this.cards.shift())
+      } else {
+        console.log('suffle')
+        console.log(this.deck)
+        console.log(this.seenCards)
+        // this.deck = this.suffle(this.seenCards.concat(this.deck))
+      }
+    },
+    suffle(deck){
+      var currentIndex = deck.length, temporaryValue, randomIndex;
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex)
+        currentIndex -= 1
+
+        // And swap it with the current element.
+        temporaryValue = deck[currentIndex]
+        deck[currentIndex] = deck[randomIndex]
+        deck[randomIndex] = temporaryValue
+      }
+      return deck
+    }
   },
   created(){
     this.setNavbarTitle("Game")
@@ -41,6 +75,7 @@ export default {
 <style lang="stylus">
 #game-page
   display flex
+  flex-direction column
   align-items center
   justify-content center
   height 100%
@@ -49,11 +84,15 @@ export default {
     display flex
     align-items center
     justify-content center
+    margin-bottom 3em
 
     .card
       font-size 3.5em
       padding 1em 3em
       background white
       box-shadow -1px 3px 2px 1px rgba(0, 0, 0, .1)
+
+  .text-input-container
+    margin-bottom 3em
 
 </style>

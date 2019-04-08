44395a8cacdce99f6e664260c1ea39a48d6e6085
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-08 05:59:35 +0000

    Deploy yintothayang/chomsky-www to github.com/yintothayang/chomsky-www.git:gh-pages

---
+++ precache-manifest.6180b65fe58c3a6abaca085fc3020df0.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "645b0e460698417e1820",
+    "url": "/css/app.a1d4c31f.css"
+  },
+  {
+    "revision": "645b0e460698417e1820",
+    "url": "/js/app.d6ded889.js"
+  },
+  {
+    "revision": "a8f7767143b2f7933d59",
+    "url": "/css/chunk-vendors.9cba8090.css"
+  },
+  {
+    "revision": "a8f7767143b2f7933d59",
+    "url": "/js/chunk-vendors.1276c661.js"
+  },
+  {
+    "revision": "a20f92cc3aaa2a47e05acac593843d84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a493ba0aa0b8ec8068d786d7248bb92c",
+    "url": "/browserconfig.xml"
+  },
+  {
+    "revision": "edcf51bdef9a37a390c89157a4929ef9",
+    "url": "/favicon-16x16.png"
+  },
+  {
+    "revision": "0e37fdf91f50b7574421d49f5729bdec",
+    "url": "/favicon-32x32.png"
+  },
+  {
+    "revision": "76f4e5a244c20f28c0b86e8d7ab6f765",
+    "url": "/safari-pinned-tab.svg"
+  },
+  {
+    "revision": "15579ed77711c9d1fdac3c31afe924fe",
+    "url": "/site.webmanifest"
+  },
+  {
+    "revision": "0e22d46ecac43a0e974df5d5ed7b143c",
+    "url": "/mstile-150x150.png"
+  },
+  {
+    "revision": "954901aefd3d39df1ecc84e01605058c",
+    "url": "/android-chrome-192x192.png"
+  },
+  {
+    "revision": "dd745eadae199d389439373efe94f278",
+    "url": "/apple-touch-icon.png"
+  },
+  {
+    "revision": "038e009ab1d31c82d226fe48a293e288",
+    "url": "/android-chrome-512x512.png"
+  }
+];
